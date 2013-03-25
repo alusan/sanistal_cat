a3d8b0fbfd882c729cfee830dd830fa5d8ab9c75
alusan <morten@all-knowing.dk>
2013-03-25 16:49:31 +0100

    Design into XHTML/CSS & cleanup
    Tested and approved in Firefox and Chrome.
    Relocated stylesheet for coding purposes and deleted useless imagefiles.

--- index.php
+++ index.php
@@ -4,10 +4,31 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<title>Sanistål</title>
+		<title>Sanistål Beklædningskatalog</title>
+		<link type="text/css" rel="stylesheet" href="desktop.css" />
 	</head>
 	<body>
-		<p>Hello world</p>
+		<div id="main">
+			<div id="topbanner">
+				<a href="index.php"><img src="design/logo.jpg" alt="Sanistål Beklædningskatalog" /></a>
+			</div>
+			<div id="searchbarMenu"></div>
+			<div id="content">
+				<div id="sidebar">
+					<p>Sidebar content</p>
+					<?php echo "<p>Sidebar content in PHP</p>"; ?>
+				</div>
+				<div id="cat_view">
+					<p>Hello world</p>
+					<?php echo "<p>Hello again in PHP</p>"; ?>
+				</div>
+				
+			</div>
+		</div>
+		
+		
+		
+		
 
 	</body>
 </html>
