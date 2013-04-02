e8e5d758eff017535d5ea838df00e34e02135170
alusan <morten@all-knowing.dk>
2013-04-02 22:52:25 +0200

    Layout for catalogueview completed
    Added new div's and tables.
    Added jsTree for the navigationsmenu.

--- index.php
+++ index.php
@@ -5,7 +5,22 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<title>Sanistål Beklædningskatalog</title>
-		<link type="text/css" rel="stylesheet" href="desktop.css" />
+		<link rel="stylesheet" href="css/lightbox.css" type="text/css" media="screen" />
+		<link rel="stylesheet" href="desktop.css" type="text/css" media="screen" />
+		<script type="text/javascript" src="lib/jquery.js"></script>
+		<script type="text/javascript" src="jquery.tree.js"></script>
+
+    <script type="text/javascript">
+	$(function () {
+		$("#navigationmenu").tree({
+			ui : {
+			theme_name : "classic"
+			}
+		});
+	});
+		
+    </script>
+    
 	</head>
 	<body>
 		<div id="main">
@@ -15,12 +30,74 @@
 			<div id="searchbarMenu"></div>
 			<div id="content">
 				<div id="sidebar">
-					<p>Sidebar content</p>
-					<?php echo "<p>Sidebar content in PHP</p>"; ?>
+    				<div id="navigationmenu">
+
+        			<ul>
+        				<li>Ford
+        					<ul>
+        						<li>Transit Connect
+        							<ul>
+        								<li>L1</li>
+        								<li>L2</li>
+        								<li>L3</li>
+        								<li>L4</li>
+        							</ul>
+        						</li>
+        						<li>Transit Minibus</li>
+        						<li>Transit Van</li>
+        					</ul>
+        				</li>
+        				<li>Citroen</li>
+        				<li>Toyota</li>
+        			</ul>
+    				</div>
 				</div>
 				<div id="cat_view">
-					<p>Hello world</p>
-					<?php echo "<p>Hello again in PHP</p>"; ?>
+					<div id="left_view">
+						<h1>Ford</h1>
+						<h2>TRANSIT CONNECT L1</h2>
+												
+						<table class="alu">
+							<tr class="aluFirstRow">
+								<td><p>Aluminiums-lister</p></td>
+								<td><p>Længde i cm.</p></td>
+							</tr>
+							<tr>
+								<td><p>L1 Vognbund</p></td>
+								<td><p>1x100 + 1x154</p></td>
+							</tr>
+						</table>
+						<p>Hello world</p>
+						<?php echo "<p>Hello again in PHP</p>"; ?>
+					</div>
+					<div id="right_view">
+						<a href="index.php"><img src="images_cars/ford_transit.jpg" alt="Ford Transit" /></a>
+					</div>
+					<div id="bottom_view">
+					</div>
+						<table class="confitting">
+							<tr>
+								<td><img src="images_fittings/3169158_loft.jpg" alt="loft" title="Sani nr. 3169158" /></td>
+								<td><img src="images_fittings/3169166_vognbund.jpg" alt="vognbund" title="Sani nr. 3169166" /></td>
+							</tr>
+							<tr>
+								<td><p>LOFT - Sani nr.: 3169158</p></td>
+								<td><p>VOGNBUND - Sani nr.: 3169166</p></td>
+							</tr>
+						</table>
+							<h3>BEKLÆDNINGER - Sani nr. 3169141</h3>
+						<table class="confitting">
+							<tr>
+								<td><img src="images_fittings/3169141_1-3.jpg" alt="Beklædning 1-3" title="Sani nr. 3169141" /></td>
+								<td><img src="images_fittings/3169141_2-3.jpg" alt="Beklædning 2-3" title="Sani nr. 3169141" /></td>
+								<td><img src="images_fittings/3169141_3-3.jpg" alt="Beklædning 3-3" title="Sani nr. 3169141" /></td>
+							</tr>
+							<tr>
+								<td><p>VENSTRE SIDE (1/3)</p></td>
+								<td><p>HØJRE SIDE (2/3)</p></td>
+								<td><p>SKYDEDØR (3/3)</p></td>
+							</tr>
+						</table>
 				</div>
 				
 			</div>
