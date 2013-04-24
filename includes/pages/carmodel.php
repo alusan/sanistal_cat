7c728ee6a4fda15407e9c6c91f4e44b8c21a3a72
Mathias Petræus <mpe3us@live.dk>
2013-04-24 11:53:23 +0200

    Page index implemented
    - added frontpage
    - added carmodel
    - added 'Peugeot Bipper' as temporary carmodel template for the carmodel
    page
    - fixed. PDF now opens in a new tab instead of the same window

---
+++ includes/pages/carmodel.php
@@ -0,0 +1,44 @@
+                        <div id="left_view">
+                        <h1>Peugeot</h1>
+						<h2>Bipper</h2>
+												
+						<table class="alu">
+							<tr class="aluFirstRow">
+								<td class="alu"><p>Aluminiums-lister</p></td>
+								<td class="alu"><p>Længde i cm.</p></td>
+							</tr>
+							<tr>
+								<td class="alu"><p>Bipper Vognbund</p></td>
+								<td class="alu"><p>1x46 + 1x107</p></td>
+							</tr>
+						</table>
+						<a href="pdf/1433317.pdf" target="_blank"><img class="extraTopMargin" src="design/button_download_pdf.jpg" alt="Download PDF" /></a>
+					</div>
+					<div id="right_view">
+						<a href="index.php"><img src="images_cars/peugeot_bipper.jpg" alt="Peugeot Bipper" /></a>
+					</div>
+					<div id="bottom_view">
+						<table class="confitting">
+							<tr>
+								<td width="50%"><img src="images_fittings/3169158_loft.jpg" alt="loft" title="Sani nr. 3169158" /></td>
+								<td width="50%"><img src="images_fittings/3169166_vognbund.jpg" alt="vognbund" title="Sani nr. 3169166" /></td>
+							</tr>
+							<tr>
+								<td><p>LOFT - Sani nr.: 3169158</p></td>
+								<td><p>VOGNBUND - Sani nr.: 3169166</p></td>
+							</tr>
+						</table>
+							<h3>BEKLÆDNINGER - Sani nr. 3169141</h3>
+						<table class="confitting">
+							<tr>
+								<td><img src="images_fittings/3169141_1-3.jpg" alt="Beklædning 1-3" title="Sani nr. 3169141" /></td>
+								<td><img src="images_fittings/3169141_2-3.jpg" alt="Beklædning 2-3" title="Sani nr. 3169141" /></td>
+								<td><img src="images_fittings/3169141_3-3.jpg" alt="Beklædning 3-3" title="Sani nr. 3169141" /></td>
+							</tr>
+							<tr>
+								<td><p>VENSTRE SIDE (1/3)</p></td>
+								<td><p>HØJRE SIDE (2/3)</p></td>
+								<td><p>SKYDEDØR (3/3)</p></td>
+							</tr>
+						</table>
+					</div>
---
+++ includes/pages/frontpage.php
@@ -0,0 +1,3 @@
+<h1>Velkommen til Beklædningskataloget</h1>
+                        
+<p>Benyt navigationsmenuen til venstre eller søgefunktion for at komme frem til den ønskede bilmodel.</p>
