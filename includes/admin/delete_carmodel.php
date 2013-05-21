19458614be8878070e28c40206f52edd9ade7978
Morten Johannsen <morten@all-knowing.dk>
2013-05-21 13:51:37 +0200

    Fixed - Delete query for carmodel

--- includes/admin/delete_carmodel.php
+++ includes/admin/delete_carmodel.php
@@ -13,6 +13,12 @@
 		
 		mysql_query("DELETE FROM biler WHERE bil_id = '$bil_id' LIMIT 1;") 
 			or die(mysql_error());
+		
+		//delete relational rows to this carmodel
+		mysql_query("DELETE FROM alulister_connect WHERE bil_id = '$bil_id';") 
+			or die(mysql_error());
+		mysql_query("DELETE FROM loft_bund_connect WHERE bil_id = '$bil_id';") 
+			or die(mysql_error());
 	}
 	
 	
